52e89995645714c86000649c3214ff4211b3e99a
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-07-11 20:16:18 -0400

    fixing rebase error

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -1064,7 +1064,7 @@ export function getNewTokensImported(state) {
   return state.appState.newTokensImported;
 }
 
-/** 
+/**
  * To check if the token detection is OFF and the network is Mainnet
  * so that the user can skip third party token api fetch
  * and use the static tokenlist from contract-metadata
@@ -1102,9 +1102,10 @@ export function getIsTokenDetectionSupported(state) {
 
 /**
  * To get the `customNetworkListEnabled` value which determines whether we use the custom network list
+ *
  * @param {*} state
  * @returns Boolean
  */
- export function getIsCustomNetworkListEnabled(state) {
+export function getIsCustomNetworkListEnabled(state) {
   return state.metamask.customNetworkListEnabled;
 }
